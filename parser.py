d30ad43d7599a350d8c687906e48b2384210b186
Дмитрий <dfeskov1998@mail.ru>
2023-11-01 23:54:23 +0300

    create SJ class and unload SJ_KEY

--- parser.py
+++ parser.py
@@ -24,8 +24,22 @@ class HhParser(BaseParser):
             self.params['page'] += 1
         return vacancies
 
+class SJ(BaseParser):
 
+    def __init__(self):
+        self.url = 'https://api.superjob.ru/2.0/vacancies/'
+        self.params = {'keyword': '', 'count': 100, 'page': 0}
+
+    def get_vacancies(self, keyword):
+        vacancies = []
+        self.params['keyword'] = keyword
+        while self.params.get('page') != 5:
+            response = requests.get(self.url, params=self.params)
+            vacancies.extend(response.json()['objects'])
+            self.params['page'] += 1
+        return vacancies
 
 hh = HhParser()
 print(hh.get_vacancies('python'))
 
+sj = SJ()
