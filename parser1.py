49e50059df0b673d0bff7818f5174f8a68de8558
Дмитрий <dfeskov1998@mail.ru>
2023-11-06 16:12:25 +0300

    recreate class Parser

--- parser1.py
+++ parser1.py
@@ -1,45 +1,47 @@
 from abc import ABC, abstractmethod
 import requests
+import os
+
 
 class BaseParser(ABC):
+    """Абстрактный класс-парсер"""
 
     @abstractmethod
-    def get_vacancies(self):
+    def get_vacancies(self, keyword):
         pass
 
-class HhParser(BaseParser):
-    """Класс для работы API HeadHunter"""
+
+class HH(BaseParser):
+    """Класс для парсинга вакансий с HH.ru"""
 
     def __init__(self):
-
         self.url = 'https://api.hh.ru/vacancies'
+        self.headers = {'User-Agent': 'HH-User-Agent'}
         self.params = {'only_with_salary': True, 'text': '', 'per_page': 100, 'page': 0}
 
     def get_vacancies(self, keyword):
         vacancies = []
         self.params['text'] = keyword
         while self.params['page'] != 20:
-            response = requests.get(self.url, params=self.params)
+            response = requests.get(self.url, params=self.params, headers=self.headers)
             vacancies.extend(response.json()['items'])
             self.params['page'] += 1
         return vacancies
 
+
 class SJ(BaseParser):
+    """Класс для парсинга вакансий с Superjob"""
 
     def __init__(self):
         self.url = 'https://api.superjob.ru/2.0/vacancies/'
+        self.headers = {'X-Api-App-Id': os.getenv('SJ_KEY')}
         self.params = {'keyword': '', 'count': 100, 'page': 0}
 
     def get_vacancies(self, keyword):
         vacancies = []
         self.params['keyword'] = keyword
         while self.params.get('page') != 5:
-            response = requests.get(self.url, params=self.params)
+            response = requests.get(self.url, headers=self.headers, params=self.params)
             vacancies.extend(response.json()['objects'])
             self.params['page'] += 1
         return vacancies
-
-hh = HhParser()
-print(hh.get_vacancies('python'))
-
-sj = SJ()
