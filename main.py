6fbf0d9c47cab603d02982cae1865ac366c928dd
Дмитрий <dfeskov1998@mail.ru>
2023-11-06 20:03:25 +0300

    correct main

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 from parser1 import HH, SJ
-from utils import get_hh_vacancies, get_sj_vacancies
+from utils import get_hh_vacancies, get_sj_vacancies, sort_vacancies
 from json_saver import JSONSaver
 
+
 def main():
+    """Основная функция программы для поиска вакансий"""
 
     print("Приветствуем, представляем парсер вакансий")
     keyword = input("Введите слово для поиска ")
+    if keyword.lower() == "выход":
+        print("Завершение программы")
     hh = HH()
     sj = SJ()
     hh_vacancies = hh.get_vacancies(keyword)
@@ -15,6 +19,11 @@ def main():
     vacancies = hh_vacancies + sj_vacancies
     vacancies_json = JSONSaver('vacancies')
     vacancies_json.write_json(vacancies)
-    print(vacancies_json.sort(reverse=True)[:2])
+    count = input("Введите количество желаемых вакансий для вывода ")
+    order = input("Отсортировать по убыванию? Да/Нет ")
+    vacancies = sort_vacancies(vacancies, int(count), order)
+    for vacancy in vacancies:
+        print(vacancy)
+
 
 main()
